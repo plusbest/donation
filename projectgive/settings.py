4dacd47b9ac87572a235be81995b6ea671095589
plusbest <jerry.wycoco@gmail.com>
2019-12-27 10:10:03 -0800

    Added local custom marker icon png files

--- projectgive/settings.py
+++ projectgive/settings.py
@@ -16,9 +16,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+
+ENV_PATH = os.path.abspath(os.path.dirname(__file__))
+MEDIA_ROOT = os.path.join(ENV_PATH, 'media/')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '8fd660ip)#ljj*muchnc_orute55&1^6dtd161u&biz_y7_)(r'
